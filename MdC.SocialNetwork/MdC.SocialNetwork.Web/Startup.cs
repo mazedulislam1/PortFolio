e2fd1830deeb367336501bfba1cc01d30ad7cf94
mazedul <mazedul001@gmail.com>
2021-05-19 22:05:07 +0600

    modified default identity to add identity made RequireConfirmedAccount true to false

--- MdC.SocialNetwork/MdC.SocialNetwork.Web/Startup.cs
+++ MdC.SocialNetwork/MdC.SocialNetwork.Web/Startup.cs
@@ -30,8 +30,12 @@ namespace MdC.SocialNetwork
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddEntityFrameworkSqlServer();
