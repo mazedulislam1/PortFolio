5a5bdf2b8d2ae1e4c1e79168edfc5ede5f98d5f8
mazedul <mazedul001@gmail.com>
2021-05-19 20:54:14 +0600

    user Identity options added in Startup file

--- MdC.SocialNetwork/MdC.SocialNetwork.Web/Data/Migrations/20210519134924__testtoSeeWhatIAdded.Designer.cs
+++ MdC.SocialNetwork/MdC.SocialNetwork.Web/Data/Migrations/20210519134924__testtoSeeWhatIAdded.Designer.cs
@@ -4,14 +4,16 @@ using MdC.SocialNetwork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MdC.SocialNetwork.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210519134924__testtoSeeWhatIAdded")]
+    partial class _testtoSeeWhatIAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MdC.SocialNetwork/MdC.SocialNetwork.Web/Startup.cs
+++ MdC.SocialNetwork/MdC.SocialNetwork.Web/Startup.cs
@@ -34,6 +34,52 @@ namespace MdC.SocialNetwork
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddEntityFrameworkSqlServer();
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                //Cookie Settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(100);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            //services.AddSingleton<UserManager<ExtendedIdentityUser>>();
+            //services.AddSingleton<SignInManager<ExtendedIdentityUser>>();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +104,13 @@ namespace MdC.SocialNetwork
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
